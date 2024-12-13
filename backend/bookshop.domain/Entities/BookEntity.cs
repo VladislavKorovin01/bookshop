ace5702d7c3b94031c8bbbec9bf29da9e00a157d
Vlad <skyline_blade@mail.ru>
2024-12-13 15:28:31 +0300

    init

---
+++ backend/bookshop.domain/Entities/BookEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookshop.domain.Entities
+{
+    public class BookEntity
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ backend/bookshop.domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using bookshop.domain.Entities;
+
+namespace bookshop.domain.Interfaces
+{
+    public interface IBookRepository
+    {
+        public void Add();
+        public Task<IEnumerable<BookEntity>> GetAll();
+        public Task<BookEntity> GetById(long id);
+        public Task Update(BookEntity book);
+        public Task Delete(long id);
+    }
+}
